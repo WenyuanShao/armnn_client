bbe7593d28f6f7e9a4d4a9c05acefdbf45c6ddef
graceliu <graceliu@node-6.6-nodes-c220g1.gwcloudlab-pg0.wisc.cloudlab.us>
2019-04-22 17:39:57 -0500

    build: fix path in makefile to clouldlab

--- NN/Debug/src/CMSIS_5/CMSIS/DSP/Examples/ARM/arm_class_marks_example/RTE/Device/ARMCM3/subdir.mk
+++ NN/Debug/src/CMSIS_5/CMSIS/DSP/Examples/ARM/arm_class_marks_example/RTE/Device/ARMCM3/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 src/CMSIS_5/CMSIS/DSP/Examples/ARM/arm_class_marks_example/RTE/Device/ARMCM3/%.o: ../src/CMSIS_5/CMSIS/DSP/Examples/ARM/arm_class_marks_example/RTE/Device/ARMCM3/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -std=c99 -DARM_MATH_CM7 -I/home/shaowy/workspace/NN/src/CMSIS_5/CMSIS/DSP/Include -I/home/shaowy/workspace/NN/src/CMSIS_5/CMSIS/Core/Include -I/home/shaowy/workspace/NN/src/CMSIS_5/CMSIS/NN/Include -O0 -g3 -Wall -c -fmessage-length=0 -Wno-unused-function -Wno-unused-variable -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc -std=c99 -DARM_MATH_CM7 -I/users/graceliu/armnn_expr/NN/src/CMSIS_5/CMSIS/DSP/Include -I/users/graceliu/armnn_expr/NN/src/CMSIS_5/CMSIS/Core/Include -I/users/graceliu/armnn_expr/NN/src/CMSIS_5/CMSIS/NN/Include -O0 -g3 -Wall -c -fmessage-length=0 -Wno-unused-function -Wno-unused-variable -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
